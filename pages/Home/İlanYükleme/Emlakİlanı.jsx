01b8da86e891589e3fd775be04f14fd4a390176a
Kerem Bozmaz <kerembozmaz@192.168.1.37>
2024-02-04 21:51:59 +0300

    carousel added

--- pages/Home/İlanYükleme/Emlakİlanı.jsx
+++ pages/Home/İlanYükleme/Emlakİlanı.jsx
@@ -1,30 +1,10 @@
-import { View, Text, SafeAreaView,StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-import DetailsPicture from '../../../components/DetailsPicture'
-import PagerView from 'react-native-pager-view';
+
 export default function Emlakİlanı() {
   return (
-    <SafeAreaView style={{flex:1}}>
-         <PagerView style={styles.viewPager} initialPage={0}>
-        <View style={styles.page} key="1">
-        <DetailsPicture/>
-        </View>
-        <View style={styles.page} key="2">
-        <DetailsPicture/>
-        </View>
-        <View style={styles.page} key="3">
-        <DetailsPicture/>
-        </View>
-      </PagerView>
-   
-    </SafeAreaView>
+    <View>
+      <Text>Emlakİlanı</Text>
+    </View>
   )
 }
-const styles = StyleSheet.create({
-  viewPager: {
-    flex: 1,
-  },
-  page: {
-   
-  },
-});
