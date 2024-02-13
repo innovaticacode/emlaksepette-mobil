6a385b5242496e8fb91951ec204f99f9de88d05c
Kerembozmaz <kerembzmz44@gmail.com>
2024-02-13 17:22:01 +0300

    Details updated

--- pages/Home/Details.jsx
+++ pages/Home/Details.jsx
@@ -6,8 +6,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
-  Button,
-  Modal,
+
+  Button, Modal,
 } from "react-native";
 import { React, useRef, useState } from "react";
 import Icon from "react-native-vector-icons/AntDesign";
@@ -16,7 +16,7 @@ import Settings from "../../components/Settings";
 import PagerView from 'react-native-pager-view';
 
 import Map from "../../components/Map";
-import Icon2 from "react-native-vector-icons/Feather";
+import Icon2 from 'react-native-vector-icons/Feather';
 import DetailsPicture from "../../components/DetailsPicture";
 import ShoppinInfo from "../../components/ShoppinInfo";
 
@@ -27,19 +27,16 @@ import Information from "../../components/Information";
 
 
 import { useRoute } from '@react-navigation/native';
-
 import Heart from "react-native-vector-icons/AntDesign";
 import Bookmark from "react-native-vector-icons/FontAwesome";
 
 export default function Details() {
   const [tabs, setTabs] = useState(0);
-  const [heart, setHeart] = useState("hearto");
-  const [bookmark, setbookmark] = useState("bookmark-o");
+  const [heart, setHeart] = useState('hearto');
+  const [bookmark, setbookmark] = useState('bookmark-o')
   const changeHeart = () => {
-    setHeart(heart === "hearto" ? "heart" : "hearto");
-  };
-  const changeBookmark = () => {
-    setbookmark(bookmark === "bookmark-o" ? "bookmark" : "bookmark-o");
+    setHeart(heart === 'hearto' ? 'heart' : 'hearto');
+   
   };
   const changeBookmark=()=>{
     setbookmark(bookmark==='bookmark-o' ? 'bookmark': 'bookmark-o')
@@ -52,62 +49,93 @@ export default function Details() {
         
     <ScrollView style={{ backgroundColor: 'white' }} indicatorStyle="white">
       <View style={{ flex: 1, height:tabs===4 ?1100:1590 }}>
-
       <View
-        style={{
-          position: "absolute",
-          zIndex: 1,
-          width: "100%",
-          height: "7%",
-
-          bottom: 30,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingRight: 20,
-          paddingLeft: 20,
-        }}
-      >
-        <TouchableOpacity
           style={{
-            width: "40%",
-            height: "90%",
-            backgroundColor: "#284ABB",
-
-            justifyContent: "center",
+            width: 50,
+            height: 150,
+            backgroundColor: "transparent",
+            position: "absolute",
+            right: 20,
+            top: 43,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            gap: 20,
+            zIndex:1
           }}
         >
-          <Text style={styles.btnText}>Ara</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            width: "40%",
-            height: "90%",
-            backgroundColor: "#284ABB",
+          <TouchableOpacity>
+            <View
+              style={{
+                backgroundColor: "#FFFFFFAD",
+                justifyContent: "center",
+                width: 40,
+                height: 40,
+                alignItems: "center",
+                borderRadius: 20,
+              }}
+            >
+              <Icon name="sharealt" size={20} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={changeHeart}>
+            <View
+              style={{
+                backgroundColor: "#FFFFFFAD",
+                width: 40,
+                height: 40,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 20,
+              }}
+            >
+              <Heart name={heart} size={20} color={heart=='hearto'?'black':'red'} />
+            </View>
+          </TouchableOpacity>
 
-            justifyContent: "center",
+          <TouchableOpacity onPress={changeBookmark}>
+            <View
+              style={{
+                backgroundColor: "#FFFFFFAD",
+                width: 40,
+                height: 40,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 20,
+              }}
+            >
+              <Bookmark name={bookmark} size={20} color={bookmark=='bookmark-o'?'black':'red'} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <PagerView style={styles.viewPager} >
+      
+          <View style={styles.page} key="1">
+            <DetailsPicture />
+          </View>
+          <View style={styles.page} key="2">
+            <DetailsPicture />
+          </View>
+          <View style={styles.page} key="3">
+            <DetailsPicture />
+          </View>
+          <View style={styles.page} key="4">
+            <DetailsPicture />
+          </View>
+        </PagerView>
+        <View
+          style={{
+            width: "100%",
+            height: 500,
+
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            top: 5,
           }}
         >
-          <Text style={styles.btnText}>Sepete Ekle</Text>
-        </TouchableOpacity>
-      </View>
-
-      <ScrollView style={{ backgroundColor: "white" }} indicatorStyle="white">
-        <View style={{ flex: 1, height: tabs === 4 ? 1100 : 1590 }}>
           <View
-            style={{
-              width: 50,
-              height: 150,
-              backgroundColor: "transparent",
-              position: "absolute",
-              right: 20,
-              top: 43,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
-              gap: 20,
-              zIndex: 1,
-            }}
+            style={{ width: '100%', height: 40, gap: 10 }}
           >
            
             <View style={{ }}>
@@ -115,51 +143,51 @@ export default function Details() {
                 <Text style={{ fontSize: 16,  }}>
                   {otherParam}
                 </Text>
+              </View>
+            </View>
+          
+            <View
+              style={{
+                backgroundColor: "grey",
+                width: "100%",
+                height: 0.4,
+                bottom: 5,
+              }}
+            ></View>
 
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={changeHeart}>
-              <View
-                style={{
-                  backgroundColor: "#FFFFFFAD",
-                  width: 40,
-                  height: 40,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: 20,
-                }}
+            <View style={{
+              width:'100%',
+              display:'flex',
+              flexDirection:'row',
+              justifyContent:'space-between',
+              bottom:7
+            }}
               >
-                <Heart
-                  name={heart}
-                  size={20}
-                  color={heart == "hearto" ? "black" : "red"}
-                />
-              </View>
-            </TouchableOpacity>
 
             <View style={{width:'50%',paddingLeft:10,}}>
               <Text style={styles.text}>{konum}</Text>
-
             </View>
-            <View style={styles.page} key="2">
-              <DetailsPicture />
+            <View style={{width:'50%',paddingRight:10, alignItems:'flex-end'}}>
+              <Text style={styles.text} >İlan No:0000000</Text>
             </View>
-            <View style={styles.page} key="3">
-              <DetailsPicture />
+           
             </View>
-            <View style={styles.page} key="4">
-              <DetailsPicture />
-            </View>
-          </PagerView>
+    
+          
+          </View>
+         
           <View
             style={{
               width: "100%",
-              height: 500,
+              height: 70,
+
+              justifyContent: 'center',
+              top: 35,
+              bottom:10,
 
               display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              top: 5,
+              flexDirection: "row",
+              gap: 5
             }}
           >
             <ScrollView horizontal indicatorStyle="white" >
@@ -189,15 +217,14 @@ export default function Details() {
               <Text  style={{ color: tabs === 1 ? "white" : "black",fontSize:12  }}>Açıklama</Text>
             </TouchableOpacity>
 
-
-              <View
-                style={{
-                  backgroundColor: "grey",
-                  width: "100%",
-                  height: 0.4,
-                  bottom: 5,
-                }}
-              ></View>
+            <TouchableOpacity
+              onPress={() => setTabs(2)}
+              style={{
+                width: '10%',
+                backgroundColor: tabs === 2 ? "#ea2a28" : "#EFEFEF",
+                alignItems: "center",
+                justifyContent: 'center',
+                height: 40
 
               }}
             >
@@ -218,10 +245,12 @@ export default function Details() {
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => setTabs(4)}
-
               style={{
-                width: "100%",
-                height: 70,
+                width: '10%',
+                backgroundColor: tabs === 4 ? "#ea2a28" : "#EFEFEF",
+                alignItems: "center",
+                justifyContent: 'center',
+                height: 40
 
               }}
             >
@@ -236,9 +265,6 @@ export default function Details() {
                 justifyContent: 'center',
                 height: 40
 
-                display: "flex",
-                flexDirection: "row",
-                gap: 5,
               }}
             >
               <Text style={{ color: tabs === 5? "white" : "black",fontSize:12  }}>Vaziyet&Kat planı</Text>
@@ -258,52 +284,59 @@ export default function Details() {
           </View>
           <View style={styles.Info}>
           <ShoppinInfo flex={  tabs===3? 'none':'flex'} bottom={tabs ===2 ?120:0} />
-  
+      
           </View>
+         
         </View>
-      </ScrollView>
+      
+      </View>
+
+    </ScrollView>
+  
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     height: 50,
-    backgroundColor: "#ecf0f1", // Top bar background color
+    backgroundColor: '#ecf0f1', // Top bar background color
   },
   segment: {
     flex: 1,
     padding: 10,
-    alignItems: "center",
+    alignItems: 'center',
   },
   selectedSegment: {
-    backgroundColor: "#3498db", // Selected segment color
+    backgroundColor: '#3498db', // Selected segment color
   },
   segmentText: {
-    color: "#2c3e50", // Segment text color
+    color: '#2c3e50', // Segment text color
   },
   viewPager: {
-    height: 250,
+    height: 250
   },
   Info: {
-    width: "100%",
+    
+    width: '100%',
     top: 20,
-    height: 240,
+    height: 240
   },
-  text: {
-    fontSize: 11,
-    fontWeight: "500",
-    fontFamily: "Verdana",
-    color: "grey",
+  text:{
+    fontSize:11,
+    fontWeight:'500',
+    fontFamily:'Verdana',
+    color:'grey'
   },
-  btnText: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "white",
-    fontWeight: "400",
-    letterSpacing: 1,
-  },
+  btnText:{
+    fontSize:20,
+    textAlign:'center',
+    color:'white',
+    fontWeight:'400',
+    letterSpacing:1
+  }
 });
