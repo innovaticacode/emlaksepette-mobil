976eee28f8f355de344f42ea25bb6fba530e90bf
aykut7 <aykutaltnsk@gmail.com>
2024-03-19 16:58:54 +0300

    asd

--- pages/Home/Login&Register/Login.jsx
+++ pages/Home/Login&Register/Login.jsx
@@ -1,145 +1,264 @@
-import { View, Text, SafeAreaView, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity ,Dimensions,Image} from 'react-native'
-import { React, useState } from 'react'
-import BackIcon from "react-native-vector-icons/AntDesign"
-import EyeIcon from "react-native-vector-icons/Ionicons"
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import IconSocialMedia from "react-native-vector-icons/AntDesign"
-import Icon from "react-native-vector-icons/Entypo"
-import { CheckBox } from 'react-native-elements';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
+import { React, useState } from "react";
+import BackIcon from "react-native-vector-icons/AntDesign";
+import EyeIcon from "react-native-vector-icons/Ionicons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import IconSocialMedia from "react-native-vector-icons/AntDesign";
+import Icon from "react-native-vector-icons/Entypo";
+import { CheckBox } from "react-native-elements";
 
-export default function Login({navigation}) {
-    const [eye, seteye] = useState('eye-off-sharp')
-    const [Show, setShow] = useState(false)
-    const show=()=>{
-        setShow(!Show)
-    }
-    const [checked, setChecked] =useState(false);
-    const toggleCheckbox = () => setChecked(!checked);
+export default function Login({ navigation }) {
+  const [eye, seteye] = useState("eye-off-sharp");
+  const [Show, setShow] = useState(false);
+  const show = () => {
+    setShow(!Show);
+  };
+  const [checked, setChecked] = useState(false);
+  const toggleCheckbox = () => setChecked(!checked);
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    
-   <View style={styles.container}>
-        <View style={styles.header}>
-
-        </View>
+      <View style={styles.container}>
+        <View style={styles.header}></View>
         <View style={styles.logIn}>
           <KeyboardAwareScrollView>
-          <View style={styles.form}>
-           
-            <View>
-              <Text style={{color:'#17243e',fontSize:20,fontWeight:'bold'}}>Giriş Yap</Text>
-            </View>
-
-              <View style={{gap:10}}>
-                <View style={{paddingLeft:10}}>
-                <Text style={{fontSize:15,color:'grey',fontWeight:600}}>E-Mail</Text>
-                </View>
-              
-                <TextInput style={styles.Input} placeholder='example@gmail.com'/>
+            <View style={styles.form}>
+              <View>
+                <Text
+                  style={{ color: "#17243e", fontSize: 20, fontWeight: "bold" }}
+                >
+                  Giriş Yap
+                </Text>
               </View>
-              <View style={{gap:10}}>
-                <View style={{paddingLeft:10}}>
-                <Text style={{fontSize:15,color:'grey',fontWeight:600}}>Şifre</Text>
+
+              <View style={{ gap: 10 }}>
+                <View style={{ paddingLeft: 10 }}>
+                  <Text
+                    style={{ fontSize: 15, color: "grey", fontWeight: 600 }}
+                  >
+                    E-Mail
+                  </Text>
                 </View>
-                  <View>
-                   
-                <TextInput style={styles.Input} placeholder='Şifrenizi Girin' secureTextEntry={Show? false:true}/>
-                <TouchableOpacity style={{position:'absolute',right:10,bottom:10}} onPress={show}>
-                      <EyeIcon name={Show? 'eye':'eye-off-sharp'} size={20} color={'#333'}/>
-                    </TouchableOpacity>
+
+                <TextInput
+                  style={styles.Input}
+                  placeholder="example@gmail.com"
+                />
+              </View>
+              <View style={{ gap: 10 }}>
+                <View style={{ paddingLeft: 10 }}>
+                  <Text
+                    style={{ fontSize: 15, color: "grey", fontWeight: 600 }}
+                  >
+                    Şifre
+                  </Text>
                 </View>
-                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',gap:15}}>
+                <View>
+                  <TextInput
+                    style={styles.Input}
+                    placeholder="Şifrenizi Girin"
+                    secureTextEntry={Show ? false : true}
+                  />
+                  <TouchableOpacity
+                    style={{ position: "absolute", right: 10, bottom: 10 }}
+                    onPress={show}
+                  >
+                    <EyeIcon
+                      name={Show ? "eye" : "eye-off-sharp"}
+                      size={20}
+                      color={"#333"}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    gap: 15,
+                  }}
+                >
                   <View>
-                  <CheckBox
-           checked={checked}
-           onPress={toggleCheckbox}
-           // Use ThemeProvider to make change for all checkbox
-           iconType="material-community"
-           checkedIcon="checkbox-marked"
-           uncheckedIcon="checkbox-blank-outline"
-           checkedColor="#EA2C2E"
-           title={'Beni Hatırla'}
-          textStyle={{fontSize:13,fontWeight:400}}
-            size={22}
-           containerStyle={{padding:0,backgroundColor:'transparent',borderWidth:0,}}
-         />
+                    <CheckBox
+                      checked={checked}
+                      onPress={toggleCheckbox}
+                      // Use ThemeProvider to make change for all checkbox
+                      iconType="material-community"
+                      checkedIcon="checkbox-marked"
+                      uncheckedIcon="checkbox-blank-outline"
+                      checkedColor="#EA2C2E"
+                      title={"Beni Hatırla"}
+                      textStyle={{ fontSize: 13, fontWeight: 400 }}
+                      size={22}
+                      containerStyle={{
+                        padding: 0,
+                        backgroundColor: "transparent",
+                        borderWidth: 0,
+                      }}
+                    />
                   </View>
-          <TouchableOpacity style={{}}>
-            <Text style={{fontSize:13,letterSpacing:0.5,color:'#5D96F1',textDecorationLine:'underline'}}>Şifrenimi Unuttun?</Text>
-          </TouchableOpacity>
-        
+                  <TouchableOpacity style={{}}>
+                    <Text
+                      style={{
+                        fontSize: 13,
+                        letterSpacing: 0.5,
+                        color: "#5D96F1",
+                        textDecorationLine: "underline",
+                      }}
+                    >
+                      Şifrenimi Unuttun?
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#EA2C2E",
+                  padding: 8,
+                  borderRadius: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    color: "white",
+                    fontWeight: "600",
+                  }}
+                >
+                  Giriş Yap
+                </Text>
+              </TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  gap: 5,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "#E7EBEE",
+                    height: 1,
+                    padding: 1,
+                    width: "40%",
+                  }}
+                />
+                <Text>OR</Text>
+                <View
+                  style={{
+                    backgroundColor: "#E7EBEE",
+                    height: 1,
+                    padding: 1,
+                    width: "40%",
+                  }}
+                />
               </View>
-              <TouchableOpacity style={{backgroundColor:'#EA2C2E',padding:8,borderRadius:10}}>
-            <Text style={{textAlign:'center',color:'white',fontWeight:'600'}}>Giriş Yap</Text>
-          </TouchableOpacity>
-          <View style={{flexDirection:'row',justifyContent:'space-evenly',alignItems:'center',gap:5}}>
-            <View style={{backgroundColor:'#E7EBEE',height:1,padding:1,width:'40%'}}/>
-            <Text>OR</Text>
-            <View style={{backgroundColor:'#E7EBEE',height:1,padding:1,width:'40%'}}/>
-          </View>
 
-              <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-                <TouchableOpacity style={{backgroundColor:'#EEF4FE',padding:5,width:'40%',height:40,borderRadius:10,flexDirection:'row',alignItems:'center',gap:10}}>
-                    <Image source={require('../../../assets/gogle.png')} style={{width:'25%',height:'100%'}} resizeMode='contain'/>
-                    <Text style={{fontWeight:'bold',color:'#333'}}>Google</Text>
+              <View
+                style={{ flexDirection: "row", justifyContent: "space-around" }}
+              >
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#EEF4FE",
+                    padding: 5,
+                    width: "40%",
+                    height: 40,
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 10,
+                  }}
+                >
+                  <Image
+                    source={require("../../../assets/gogle.png")}
+                    style={{ width: "25%", height: "100%" }}
+                    resizeMode="contain"
+                  />
+                  <Text style={{ fontWeight: "bold", color: "#333" }}>
+                    Google
+                  </Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{backgroundColor:'#EEF4FE',padding:5,width:'40%',height:40,borderRadius:10,flexDirection:'row',alignItems:'center',gap:10}}>
-                    <Icon name='facebook-with-circle'size={30} color={'#1A77F3'}/>
-                    <Text style={{fontWeight:'bold',color:'#333'}}>Facebook</Text>
-               </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#EEF4FE",
+                    padding: 5,
+                    width: "40%",
+                    height: 40,
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 10,
+                  }}
+                >
+                  <Icon
+                    name="facebook-with-circle"
+                    size={30}
+                    color={"#1A77F3"}
+                  />
+                  <Text style={{ fontWeight: "bold", color: "#333" }}>
+                    Facebook
+                  </Text>
+                </TouchableOpacity>
               </View>
+            </View>
+            <View>
+              <Text style={{ textAlign: "center" }}>
+                <Text style={{ fontSize: 13, color: "#333" }}>
+                  Henüz Üye Değilmisiniz?{" "}
+                </Text>
 
-          </View>
-          <View >
-            <Text style={{textAlign:'center'}}>
-              <Text style={{fontSize:13,color:'#333'}}>Henüz Üye Değilmisiniz? </Text>
-           
-              <Text style={{fontWeight:'bold',}} onPress={()=>{
-              navigation.navigate('Register')
-              }}>Üye Ol</Text>
-            
-            
-            </Text>
-          </View>
+                <Text
+                  style={{ fontWeight: "bold" }}
+                  onPress={() => {
+                    navigation.navigate("Register");
+                  }}
+                >
+                  Üye Ol
+                </Text>
+              </Text>
+            </View>
           </KeyboardAwareScrollView>
-        
         </View>
-   </View>
-  
+      </View>
     </TouchableWithoutFeedback>
-  )
+  );
 }
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
-  
   container: {
-      flex:1,
-     backgroundColor:'white'
+    flex: 1,
+    backgroundColor: "white",
   },
-  header:{
-    flex:0.2,
-
+  header: {
+    flex: 0.2,
   },
-  logIn:{
-    backgroundColor:'#FFFFFF',
-    flex:1.8,
-    borderTopLeftRadius:20,
-    borderTopRightRadius:20,
-    position:''
+  logIn: {
+    backgroundColor: "#FFFFFF",
+    flex: 1.8,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    position: "",
   },
-  form:{
-    padding:40,
-    gap:20
+  form: {
+    padding: 40,
+    gap: 20,
   },
-  Input:{
-    padding:10,
-    borderWidth:1,
-    borderColor:'#ebebeb',
-    borderRadius:10,
-    fontSize:14
-  }
- 
- 
-
-})
+  Input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ebebeb",
+    borderRadius: 10,
+    fontSize: 14,
+  },
+});
