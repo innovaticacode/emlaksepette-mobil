9180789e0c76d74dfb2b5f33aa546807dfa9d7b2
aykut7 <aykutaltnsk@gmail.com>
2024-03-26 10:15:17 +0300

    menuIcon

--- components/SliderBar.jsx
+++ components/SliderBar.jsx
@@ -1,70 +1,59 @@
-import { View, Text ,StyleSheet,ScrollView, FlatList} from 'react-native'
-import React,{useState,useEffect} from 'react'
-import SliderItem from './SliderItem'
-import axios from 'axios';
-import SliderItemSkeleton from './SkeletonComponents/SliderItemSkeleton';
-
+import { View, Text, StyleSheet, ScrollView, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import SliderItem from "./SliderItem";
+import axios from "axios";
+import SliderItemSkeleton from "./SkeletonComponents/SliderItemSkeleton";
 
 export default function SliderBar() {
-  const apiUrl='https://emlaksepette.com/';
-  const [loading, setloading] = useState(false)
+  const apiUrl = "https://emlaksepette.com/";
+  const [loading, setloading] = useState(false);
   const [featuredStores, setFeaturedStores] = useState([]);
 
   const fetchFeaturedStores = async () => {
     try {
-      const response = await axios.get('https://emlaksepette.com/api/featured-stores');
-      setFeaturedStores(response.data)
-      setloading(true)
+      const response = await axios.get(
+        "https://emlaksepette.com/api/featured-stores"
+      );
+      setFeaturedStores(response.data);
+      setloading(true);
     } catch (error) {
       console.log(error);
     }
   };
 
-
   useEffect(() => {
-   fetchFeaturedStores()
-
+    fetchFeaturedStores();
   }, []);
   return (
-
- 
-   <ScrollView horizontal showsHorizontalScrollIndicator={false}  style={{
-        
-    top:10
-   }}>
-
-
-
-     {
-       featuredStores.map((item,index)=>(
-       
-         <View style={{width:100,alignItems:'center'}} key={index}>
-           {
-             loading==false?
-             <>
-             <SliderItemSkeleton/>
-         </>:
-             <>
-             <SliderItem key={index} image={`${apiUrl}/storage/profile_images/${item.profile_image}`} />
-         <Text numberOfLines={2} style={{fontSize:12}}>{item.name}</Text>
-          
-         </>
-           }
-
-         </View>
-           
-       ))
-     }
-    
-   </ScrollView>  
-   
-   
-  )
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={{
+        top: 0,
+      }}
+    >
+      {featuredStores.map((item, index) => (
+        <View style={{ width: 80, alignItems: "center" }} key={index}>
+          {loading == false ? (
+            <>
+              <SliderItemSkeleton />
+            </>
+          ) : (
+            <>
+              <SliderItem
+                key={index}
+                image={`${apiUrl}/storage/profile_images/${item.profile_image}`}
+              />
+              <Text numberOfLines={2} style={{ fontSize: 12 }}>
+                {item.name}
+              </Text>
+            </>
+          )}
+        </View>
+      ))}
+    </ScrollView>
+  );
 }
 const styles = StyleSheet.create({
-    scrollView: {
-        marginTop: 20,
-      },
-    
-     
-   })
+  scrollView: {},
+});
--- components/SliderMenu.jsx
+++ components/SliderMenu.jsx
@@ -1,77 +1,113 @@
-import { View, Text,ScrollView, TouchableOpacity, StyleSheet } from 'react-native'
-import React,{useState} from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import React, { useState } from "react";
 
-export default function SliderMenu({goToSlide,tab,settab}) {
-
+export default function SliderMenu({ goToSlide, tab, settab }) {
   return (
-    <ScrollView horizontal={true}  showsHorizontalScrollIndicator={false} style={{
-        
-      
-      }}>
-      <View style={{padding:10,flexDirection:'row',gap:10}}>
-      <TouchableOpacity style={[styles.tabBtn,{
-        backgroundColor:tab==0 ?'#EA2C2E':'#ebebeb',
-        borderWidth:tab==0 ? 0 :1
-      }]} 
-        onPress={()=>{
-          goToSlide(0)
-         
-        }}
-      >
-        <Text style={{textAlign:'center',color:tab==0?'white':'#EA2C2E'}}>Projeler</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={[styles.tabBtn,{
-         backgroundColor:tab==1 ?'#EA2C2E':'#ebebeb',
-         borderWidth:tab==1 ? 0 :1
-       }]}
-        onPress={()=>{
-          goToSlide(1)
-      
-        }}
-       >
-        <Text style={{textAlign:'center',color:tab==1?'white':'#EA2C2E'}}>Konut</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={[styles.tabBtn,{
-          backgroundColor:tab==2 ?'#EA2C2E':'#ebebeb',
-          borderWidth:tab==2 ? 0 :1
-       }]}
-       onPress={()=>{
-        goToSlide(2)
-       }}>
-        <Text style={{textAlign:'center',color:tab==2?'white':'#EA2C2E'}}>İş Yeri</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={styles.tabBtn}>
-        <Text style={{textAlign:'center',color:'#EA2C2E'}}>Arsa</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={styles.tabBtn}>
-        <Text style={{textAlign:'center',color:'#EA2C2E'}}>Prefabrik</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={styles.tabBtn}>
-        <Text style={{textAlign:'center',color:'#EA2C2E'}}>Tatilini Kirala</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={styles.tabBtn}>
-        <Text style={{textAlign:'center',color:'#EA2C2E'}}>Al Sat Acil</Text>
-       </TouchableOpacity>
-     
-
+    <ScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={{}}
+    >
+      <View style={{ padding: 10, flexDirection: "row", gap: 10 }}>
+        <TouchableOpacity
+          style={[
+            styles.tabBtn,
+            {
+              backgroundColor: tab == 0 ? "#EA2C2E" : "#ebebeb",
+              borderWidth: tab == 0 ? 0 : 1,
+            },
+          ]}
+          onPress={() => {
+            goToSlide(0);
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: tab == 0 ? "white" : "#333333",
+            }}
+          >
+            Projeler
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.tabBtn,
+            {
+              backgroundColor: tab == 1 ? "#EA2C2E" : "#ebebeb",
+              borderWidth: tab == 1 ? 0 : 1,
+            },
+          ]}
+          onPress={() => {
+            goToSlide(1);
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: tab == 1 ? "white" : "#333333",
+            }}
+          >
+            Konut
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.tabBtn,
+            {
+              backgroundColor: tab == 2 ? "#EA2C2E" : "#ebebeb",
+              borderWidth: tab == 2 ? 0 : 1,
+            },
+          ]}
+          onPress={() => {
+            goToSlide(2);
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: tab == 2 ? "white" : "#333333",
+            }}
+          >
+            İş Yeri
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabBtn}>
+          <Text style={{ textAlign: "center", color: "#333333" }}>Arsa</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabBtn}>
+          <Text style={{ textAlign: "center", color: "#333333" }}>
+            Prefabrik
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabBtn}>
+          <Text style={{ textAlign: "center", color: "#333333" }}>
+            Tatilini Kirala
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabBtn}>
+          <Text style={{ textAlign: "center", color: "#333333" }}>
+            Al Sat Acil
+          </Text>
+        </TouchableOpacity>
       </View>
-     
-      
-  
-      
-      
-      
-      </ScrollView> 
-  )
+    </ScrollView>
+  );
 }
-const styles=StyleSheet.create({
-    tabBtn:{
-        backgroundColor:'#ebebeb',
-        paddingLeft:15,
-        paddingRight:15,
-        padding:5,
-          borderRadius:4,
-          borderWidth:1,
-          borderColor:'#c7c7ca'
-    }
-})
+const styles = StyleSheet.create({
+  tabBtn: {
+    backgroundColor: "#ebebeb",
+    paddingLeft: 15,
+    paddingRight: 15,
+    padding: 5,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#c7c7ca",
+  },
+});
