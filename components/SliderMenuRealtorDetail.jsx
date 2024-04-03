d9f771ddceacf4f0eaf0cb23c108dbab3730102d
Kerembozmaz <kerembzmz44@gmail.com>
2024-04-03 10:26:16 +0300

    update

---
+++ components/SliderMenuRealtorDetail.jsx
@@ -0,0 +1,64 @@
+import { View, Text,ScrollView,TouchableOpacity,StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function SliderMenuDetails({tab,setTab,changeTab}) {
+    const menuItems=[
+        {text:'Projedeki Konutlar'},
+        {text:'Açıklama'},
+        {text:'Özellikler'},
+        {text:'Harita'},
+        {text:'Vaziyet & Kat Planı'}
+    ]
+  return (
+    <ScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={{}}
+      bounces={false}
+    >
+      <View style={{ padding: 10, flexDirection: "row", gap: 10 }}>
+        {menuItems.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.tabBtn,
+              {
+                backgroundColor: tab == index ? "#EA2C2E" : "white",
+                borderWidth: tab == index ? 0 : 1,
+              },
+            ]}
+            onPress={()=>{
+                changeTab(index)
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: tab == index ? "white" : "#333",
+                fontSize: 12,
+              }}
+            >
+              {item.text}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
+  )
+}
+const styles = StyleSheet.create({
+    tabBtn: {
+      backgroundColor: "white",
+      paddingLeft: 15,
+      paddingRight: 15,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 7,
+      borderRadius: 6,
+      borderWidth: 1,
+      borderColor: "#ebebeb",
+    },
+    text: {
+      fontSize: 12,
+    },
+  });
