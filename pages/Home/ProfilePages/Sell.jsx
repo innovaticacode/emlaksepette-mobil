16cc93d8ce4d916a17c61c161ae0d629165561e2
aykut7 <aykutaltnsk@gmail.com>
2024-05-20 14:34:16 +0300

    asd

--- pages/Home/ProfilePages/Sell.jsx
+++ pages/Home/ProfilePages/Sell.jsx
@@ -1,160 +1,248 @@
-import { View, Text, StyleSheet, TouchableOpacity, Keyboard,ScrollView } from 'react-native'
-import {useState} from 'react'
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { SearchBar } from '@rneui/base';
-import Icon from "react-native-vector-icons/AntDesign"
-import Order from './profileComponents/Order';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { SearchBar } from "@rneui/base";
+import Icon from "react-native-vector-icons/AntDesign";
+import Order from "./profileComponents/Order";
 export default function Sell() {
   const [search, setSearch] = useState("");
-  const [Tabs, setTabs] = useState(0)
+  const [Tabs, setTabs] = useState(0);
   const updateSearch = (search) => {
     setSearch(search);
   };
   const route = useRoute();
 
-
   const [products, setProducts] = useState([
-    { id: 1,  date: '2023-01-15' },
-    { id: 2,  date: '2023-03-10' },
-    { id: 3,  date: '2022-12-05' }
-  ])
+    { id: 1, date: "2023-01-15" },
+    { id: 2, date: "2023-03-10" },
+    { id: 3, date: "2022-12-05" },
+  ]);
 
-//  const date = new Date();
-//  const dayName = date.toLocaleDateString('tr-TR', { weekday: 'long' });
-//  const monthName = date.toLocaleDateString('tr-TR', { month: 'long' });
-  
-//  console.log("Gün: ", dayName);
+  //  const date = new Date();
+  //  const dayName = date.toLocaleDateString('tr-TR', { weekday: 'long' });
+  //  const monthName = date.toLocaleDateString('tr-TR', { month: 'long' });
 
- 
+  //  console.log("Gün: ", dayName);
+  const [sells, setSells] = useState([]);
 
-  return ( 
-    <View style={style.container} onTouchStart={()=>Keyboard.dismiss()}>
-      
-        <View style={style.Navbar}>
-            <View style={style.SearchInput}>
-              <SearchBar
-                 placeholder="Ara..."
-                 onChangeText={updateSearch}
-                 value={search}
-               containerStyle={{
-                 backgroundColor:'transparent',
-                 width:'100%',
-                 borderTopColor:'white',
-                 borderBottomColor:'white',
-                 height:60,
-                 
-                 }}
-                 searchIcon={{size:25}}
-                inputContainerStyle={{backgroundColor:'#e5e5e5',borderRadius:7,height:'100%'}}
-                 inputStyle={{fontSize:15}}
-                showCancel='false'
-                placeholderTextColor={'grey'}
+  const fetchPermissionUser = async () => {
+    try {
+      if (user.access_token) {
+        const response = await axios.get(
+          `https://test.emlaksepette.com/api/institutional/users/${user?.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user?.access_token}`,
+            },
+          }
+        );
+        setSells(response.data.boughts);
+        console.log(permissionsUser + "sadsdfsf");
+      }
+    } catch (error) {
+      console.error("eror", error);
+    }
+  };
+
+  return (
+    <View style={style.container} onTouchStart={() => Keyboard.dismiss()}>
+      <View style={style.Navbar}>
+        <View style={style.SearchInput}>
+          <SearchBar
+            placeholder="Ara..."
+            onChangeText={updateSearch}
+            value={search}
+            containerStyle={{
+              backgroundColor: "transparent",
+              width: "100%",
+              borderTopColor: "white",
+              borderBottomColor: "white",
+              height: 60,
+            }}
+            searchIcon={{ size: 25 }}
+            inputContainerStyle={{
+              backgroundColor: "#e5e5e5",
+              borderRadius: 7,
+              height: "100%",
+            }}
+            inputStyle={{ fontSize: 15 }}
+            showCancel="false"
+            placeholderTextColor={"grey"}
+          />
+        </View>
+        <View style={style.ListIcon}>
+          <TouchableOpacity
+            style={{ backgroundColor: "#e5e5e5", padding: 10, borderRadius: 6 }}
+          >
+            <View style={{ flexDirection: "row", gap: -7 }}>
+              <Icon
+                name="arrowup"
+                size={20}
+                style={{ bottom: 6 }}
+                color={"grey"}
+              />
+              <Icon
+                name="arrowdown"
+                size={20}
+                style={{ top: 3 }}
+                color={"grey"}
               />
             </View>
-            <View style={style.ListIcon}>
-              <TouchableOpacity style={{backgroundColor:'#e5e5e5',padding:10,borderRadius:6}}>
-                 <View style={{flexDirection:'row',gap:-7}}>
-                  <Icon name='arrowup' size={20} style={{bottom:6}} color={'grey'}/>
-                  <Icon name='arrowdown' size={20} style={{top:3}} color={'grey'}/>
-                 </View>  
-                 </TouchableOpacity>
-            </View>
+          </TouchableOpacity>
         </View>
-        <View style={style.TabBar}>
+      </View>
+      <View style={style.TabBar}>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <View style={{display:'flex',flexDirection:'row',gap:20}}>
-            <TouchableOpacity style={[style.TabBarBtn,{backgroundColor:Tabs==0? '#ebebeb':'#E54242',borderWidth:Tabs==0? 1:0, borderColor:'#E54242'}]}
-              onPress={()=>setTabs(0)}
+          <View style={{ display: "flex", flexDirection: "row", gap: 20 }}>
+            <TouchableOpacity
+              style={[
+                style.TabBarBtn,
+                {
+                  backgroundColor: Tabs == 0 ? "#ebebeb" : "#E54242",
+                  borderWidth: Tabs == 0 ? 1 : 0,
+                  borderColor: "#E54242",
+                },
+              ]}
+              onPress={() => setTabs(0)}
             >
-              <Text style={[style.tabBarText,{color:Tabs===0? '#E54242':'white',fontWeight:Tabs===0?'600':'normal'}]}>Tümü</Text>
+              <Text
+                style={[
+                  style.tabBarText,
+                  {
+                    color: Tabs === 0 ? "#E54242" : "white",
+                    fontWeight: Tabs === 0 ? "600" : "normal",
+                  },
+                ]}
+              >
+                Tümü
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={[style.TabBarBtn,{backgroundColor:Tabs==1? '#ebebeb':'#E54242',borderWidth:Tabs==1? 1:0, borderColor:'#E54242'}]}
-            onPress={()=>setTabs(1)}
+            <TouchableOpacity
+              style={[
+                style.TabBarBtn,
+                {
+                  backgroundColor: Tabs == 1 ? "#ebebeb" : "#E54242",
+                  borderWidth: Tabs == 1 ? 1 : 0,
+                  borderColor: "#E54242",
+                },
+              ]}
+              onPress={() => setTabs(1)}
             >
-              <Text style={[style.tabBarText,{color:Tabs===1? '#E54242':'white',fontWeight:Tabs===1?'600':'normal'}]}>Onay Bekleyenler</Text>
+              <Text
+                style={[
+                  style.tabBarText,
+                  {
+                    color: Tabs === 1 ? "#E54242" : "white",
+                    fontWeight: Tabs === 1 ? "600" : "normal",
+                  },
+                ]}
+              >
+                Onay Bekleyenler
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={[style.TabBarBtn,{backgroundColor:Tabs==2? '#ebebeb':'#E54242',borderWidth:Tabs==2? 1:0, borderColor:'#E54242'}]}
-              onPress={()=>setTabs(2)}
+            <TouchableOpacity
+              style={[
+                style.TabBarBtn,
+                {
+                  backgroundColor: Tabs == 2 ? "#ebebeb" : "#E54242",
+                  borderWidth: Tabs == 2 ? 1 : 0,
+                  borderColor: "#E54242",
+                },
+              ]}
+              onPress={() => setTabs(2)}
             >
-              <Text style={[style.tabBarText,{color:Tabs===2? '#E54242':'white',fontWeight:Tabs===2?'600':'normal'}]}>İptaller</Text>
+              <Text
+                style={[
+                  style.tabBarText,
+                  {
+                    color: Tabs === 2 ? "#E54242" : "white",
+                    fontWeight: Tabs === 2 ? "600" : "normal",
+                  },
+                ]}
+              >
+                İptaller
+              </Text>
             </TouchableOpacity>
-                 
-            </View>
+          </View>
         </ScrollView>
-        </View>
-        <ScrollView>
+      </View>
+      <ScrollView>
         <View style={style.orders}>
-                  <Order display={'none'} text={'deneme'}/>
-                  {/* <Order display={displayInfo} text={text}/>
+          {sells.map((sell, i) => (
+            <Order sell={sell} display={"none"} text={"deneme"} />
+          ))}
+
+          {/* <Order display={displayInfo} text={text}/>
                   <Order display={displayInfo} text={text}/>
                   <Order display={displayInfo} text={text}/>
                   <Order display={displayInfo} text={text}/>
                   <Order display={displayInfo} text={text}/>
                   <Order display={displayInfo} text={text}/> */}
-                 
-
         </View>
-        </ScrollView>
+      </ScrollView>
     </View>
-  )
+  );
 }
-const style=StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor:'white'
-    },
-    Navbar:{
-      width:'100%',
-      borderBottomWidth:1,
-      borderBottomColor:'#ebebeb',
-      padding:2,
-      display:"flex",
-      flexDirection:'row',
-      backgroundColor: '#FFFF',  
-      borderColor:'#e6e6e6',
-      ...Platform.select({
-          ios: {
-            shadowColor: ' #e6e6e6',
-            shadowOffset: { width: 1, height: 1 },
-            shadowOpacity: 0.1,
-            shadowRadius: 5,
-          },
-          android: {
-            elevation: 5,
-          },
-        })
-      
-    },
-    SearchInput:{
-      flex:1.7/2,
-     
-      padding:2
-    },
-    ListIcon:{
-      flex:0.3/2,
-   
-      borderBottomColor:'#e5e5e5',
-      padding:2,
-      alignItems:'center',
-      justifyContent:'center'
-    },
-    TabBar:{
-      padding:10,
-   
-      alignItems: "center",
-      justifyContent: "center",
-    
-     
-    },
-    TabBarBtn:{
-      backgroundColor:'red',
-      padding:10,
-      borderRadius:4,
-      
-    },
-    orders:{
-      width:'100%',
-      padding:15,
-      gap:15
-    }
-})
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  Navbar: {
+    width: "100%",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ebebeb",
+    padding: 2,
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#FFFF",
+    borderColor: "#e6e6e6",
+    ...Platform.select({
+      ios: {
+        shadowColor: " #e6e6e6",
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  SearchInput: {
+    flex: 1.7 / 2,
+
+    padding: 2,
+  },
+  ListIcon: {
+    flex: 0.3 / 2,
+
+    borderBottomColor: "#e5e5e5",
+    padding: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  TabBar: {
+    padding: 10,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  TabBarBtn: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 4,
+  },
+  orders: {
+    width: "100%",
+    padding: 15,
+    gap: 15,
+  },
+});
--- pages/Home/ProfilePages/profileComponents/Order.jsx
+++ pages/Home/ProfilePages/profileComponents/Order.jsx
@@ -1,104 +1,136 @@
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/SimpleLineIcons'
-import FeatherIcon from 'react-native-vector-icons/Feather'
-import StarIcon from 'react-native-vector-icons/FontAwesome'
-import { useNavigation } from '@react-navigation/native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/SimpleLineIcons";
+import FeatherIcon from "react-native-vector-icons/Feather";
+import StarIcon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import { getValueFor } from "../../../../components/methods/user";
 
-export default function Order({display,text}) {
-    const navigation=useNavigation()
+export default function Order({ display, text }) {
+  const navigation = useNavigation();
+  const [user, setUser] = useState({});
+  const [İsLoggedIn, setisLoggedIn] = useState(true);
+
+  useEffect(() => {
+    getValueFor("user", setUser);
+  }, []);
   return (
-    <TouchableOpacity onPress={()=>navigation.navigate('OrderDetail',{display,text})}>
-    <View style={style.container}>
-
-   
+    <TouchableOpacity
+      onPress={() => navigation.navigate("OrderDetail", { display, text })}
+    >
+      <View style={style.container}>
         <View style={style.InfoDateButton}>
-            <View style={style.Info}>
-              <Text>28 Şubat 2024</Text>
-              <View style={{flexDirection:'row'}}>
+          <View style={style.Info}>
+            <Text>28 Şubat 2024</Text>
+            <View style={{ flexDirection: "row" }}>
               <Text>Toplam: </Text>
-              <Text style={{color:'green'}}>2.500.000</Text>
-              </View>
-             
+              <Text style={{ color: "green" }}>2.500.000</Text>
             </View>
-            <View style={style.Button}>
-                <Text style={{color:'green',fontSize:13}}>Detaylar</Text>
-                <Icon name='arrow-right' color={'green'} size={10}/>
-            </View>
+          </View>
+          <View style={style.Button}>
+            <Text style={{ color: "green", fontSize: 13 }}>Detaylar</Text>
+            <Icon name="arrow-right" color={"green"} size={10} />
+          </View>
         </View>
 
         <View style={style.PhotoAndComment}>
-            <View style={{display:'flex', width:'100%', flexDirection:'row',justifyContent:'space-between'}}>
-            <View style={{flexDirection:'row',alignItems:'center',gap:5}}>
-                <FeatherIcon name='check' color={'green'}/>
-                <Text style={{color:'green',fontSize:13}}>Onaylandı</Text>
+          <View
+            style={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <FeatherIcon name="check" color={"green"} />
+              <Text style={{ color: "green", fontSize: 13 }}>Onaylandı</Text>
             </View>
-               <View>
-                    <TouchableOpacity  style={{borderWidth:1 ,borderColor:'#ebebeb', borderRadius:4,padding:5,flexDirection:'row',alignItems:'center',gap:7}}>
-                    <StarIcon name='star' size={14} color={'green'}/>
-                        <Text style={{color:'#353030',fontSize:12}}>Değerlendir</Text>
-                    </TouchableOpacity>
-               </View>
+            <View>
+              <TouchableOpacity
+                style={{
+                  borderWidth: 1,
+                  borderColor: "#ebebeb",
+                  borderRadius: 4,
+                  padding: 5,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 7,
+                }}
+              >
+                <StarIcon name="star" size={14} color={"green"} />
+                <Text style={{ color: "#353030", fontSize: 12 }}>
+                  Değerlendir
+                </Text>
+              </TouchableOpacity>
             </View>
+          </View>
 
-            <View style={{gap:9}}>
-           <View style={{height:80,width:'24%', }}>
-            <ImageBackground source={require('./home.jpg')} style={{width:'100%',height:'100%'}} resizeMode='cover'/>
-           </View>
-            <Text style={{color:'grey',fontSize:12}}>1 Ürün Teslim Edildi</Text>
-           </View>
-
+          <View style={{ gap: 9 }}>
+            <View style={{ height: 80, width: "24%" }}>
+              <ImageBackground
+                source={require("./home.jpg")}
+                style={{ width: "100%", height: "100%" }}
+                resizeMode="cover"
+              />
+            </View>
+            <Text style={{ color: "grey", fontSize: 12 }}>
+              1 Ürün Teslim Edildi
+            </Text>
+          </View>
         </View>
-   
-    </View>
+      </View>
     </TouchableOpacity>
-  )
+  );
 }
-const style=StyleSheet.create({
-    container:{
-        width:'100%',
-       
-        padding:2,
-        borderWidth:1.5,
-        borderRadius:5,
-        backgroundColor: '#FFFF',  
-        borderColor:'#e6e6e6',
-        ...Platform.select({
-            ios: {
-              shadowColor: ' #e6e6e6',
-              shadowOffset: { width: 1, height: 1 },
-              shadowOpacity: 0.1,
-              shadowRadius: 5,
-            },
-            android: {
-              elevation: 5,
-            },
-          })
-    },
-    InfoDateButton:{
-        borderBottomWidth:1,
-        borderBottomColor:'#ebebeb',
-        padding:10,
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between'
-    },
-    PhotoAndComment:{
-        padding:10,
-        display:'flex',
-      
-       
-      
-    },
-    Info:{
-      gap:5
-       
-    },
-    Button:{
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        gap:8
-    }
+const style = StyleSheet.create({
+  container: {
+    width: "100%",
 
-})
+    padding: 2,
+    borderWidth: 1.5,
+    borderRadius: 5,
+    backgroundColor: "#FFFF",
+    borderColor: "#e6e6e6",
+    ...Platform.select({
+      ios: {
+        shadowColor: " #e6e6e6",
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  InfoDateButton: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#ebebeb",
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  PhotoAndComment: {
+    padding: 10,
+    display: "flex",
+  },
+  Info: {
+    gap: 5,
+  },
+  Button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+});
