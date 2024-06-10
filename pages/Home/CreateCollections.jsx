d212cffb5006f47224d48a3ec1dc4a39ca4ac077
Kerembozmaz <kerembzmz44@gmail.com>
2024-06-10 18:23:33 +0300

    ana sayfada optimizasyon ve hızlandırma yapıldı

--- pages/Home/CreateCollections.jsx
+++ pages/Home/CreateCollections.jsx
@@ -1,13 +1,23 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 
 export default function CreateCollections() {
     const route=useRoute()
     const {id}= route.params
+    const [CollectionName, setCollectionName] = useState('')
   return (
     <ScrollView style={styles.container}>
-            <View></View>
+            <View style={{gap:5,paddingTop:10}}> 
+              <Text style={{fontSize:13,color:'#333',fontWeight:'bold'}}>Koleksiyon İsmi</Text>
+              <TextInput style={styles.Input} value={CollectionName} onChangeText={(value)=>setCollectionName(value)}/>
+              <View style={{paddingTop:10,alignItems:'center'}}>
+                <TouchableOpacity style={{backgroundColor:'#EA2B2E',padding:9,borderRadius:5,width:'90%'}}>
+                  <Text style={{color:'#ffffff',fontSize:13,fontWeight:'bold',textAlign:'center'}}>Koleksiyon Ekle</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          
     </ScrollView>
   )
 }
@@ -17,5 +27,10 @@ const styles=StyleSheet.create({
         flex:1,
         backgroundColor:'#f7f7f7f7',
         padding:10
-    }
+    },
+      Input:{
+        backgroundColor:'#ebebebeb',
+        padding:9,
+        borderRadius:5
+      }
 })
