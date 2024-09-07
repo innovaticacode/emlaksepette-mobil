459633e10afc4a18e9f82b13867439aa6db3f096
Okan İLBAY <okan.ilbay@hotmail.com>
2024-09-07 18:35:26 +0300

    Açılır Sol Menü İşlemleri task 3 maddesi tamamlandı

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,62 +1,61 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-export default function Categories({
-  category,
-  ıconDisplay,
-  showImage,
-  bordernone,
-  ıconName,
-}) {
+export default function Categories({ category, iconName }) {
+
+  if (category == "Projeler") {
+    iconName="folder-home";
+  }
+  else if (category == "Konut") {
+    iconName="home-group";
+  }
+  else if (category == "İş Yeri") {
+    iconName="storefront";
+  }
+  else if (category == "Arsa") {
+    iconName="terrain";
+  }
+  else if (category == "Tiny House") {
+    iconName="hoop-house";
+  }
+  else if (category == "Bungalov") {
+    iconName="tent";
+  }
+  else if (category == "Prefabrik Yapılar") {
+    iconName="greenhouse";
+  }
+  else if (category == "Tatil Sepette") {
+    iconName="island";
+  }
+
+
   return (
-    <View
-      style={[
-        styles.category,
-        {
-          borderBottomWidth: 0
-        },
-      ]}
-    > 
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap:8
-        }}
-      >
-        
-        <Icon name={ıconName} size={18}/>
-        <Text
-          style={{
-            fontSize: 14,
-            color: "#333",
-            fontWeight: "500",
-            
-          }}
-        >
-          {category}
-        </Text>
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-     
+    <View style={styles.category}>
+      <View style={styles.iconTextContainer}>
+        <Icon name={iconName} size={18} color="#333" />
+        <Text style={styles.categoryText}>{category}</Text>
       </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   category: {
     padding: 8,
-    
-    display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     borderBottomWidth: 0,
     borderBottomColor: "#ebebeb",
+  },
+  iconTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  categoryText: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "500",
   },
 });
