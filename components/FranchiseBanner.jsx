20a072609253d000f02fc5d45c3db1687e4631c9
Öztürk Şirin <ozturksirininfo@gmail.com>
2024-09-17 17:16:18 +0300

    FEAT: FranchiseBanner component and add alt text to the image

---
+++ components/FranchiseBanner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import TestFranchise from "../assets/test-franchise.png";
+
+const FranchiseBanner = (props) => {
+  const { image, text } = props;
+  return (
+    <React.Fragment>
+      <View style={styles.container}>
+        <Image
+          source={image ? image : TestFranchise}
+          style={styles.image}
+          resizeMode="contain"
+          alt="Franchise Banner"
+        />
+        <Text style={styles.text}>{text ? text : "Franchise"}</Text>
+      </View>
+    </React.Fragment>
+  );
+};
+
+export default FranchiseBanner;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgb(228, 228, 228)",
+    width: 110,
+    height: "auto",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 6,
+  },
+  image: {
+    width: 90,
+    height: 100,
+    borderRadius: 10,
+  },
+  text: {
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 8,
+  },
+});
