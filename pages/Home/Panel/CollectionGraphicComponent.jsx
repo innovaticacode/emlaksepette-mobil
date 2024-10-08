8fe0b235b2f26b1ea185ec030519b28a07c2dfe9
Öztürk Şirin <ozturksirininfo@gmail.com>
2024-10-08 10:26:47 +0300

    Refactor code to improve readability and consistency

--- pages/Home/Panel/CollectionGraphicComponent.jsx
+++ pages/Home/Panel/CollectionGraphicComponent.jsx
@@ -1,14 +1,20 @@
-import { View } from 'react-native';
-import React from 'react';
-import Svg, { Circle, Text, G, Path } from 'react-native-svg';
+import { View } from "react-native";
+import React from "react";
+import Svg, { Circle, Text, G, Path } from "react-native-svg";
 
-export default function CollectionGraphicComponent({ size, strokeWidth, color1, color2, text }) {
+export default function CollectionGraphicComponent({
+  size,
+  strokeWidth,
+  color1,
+  color2,
+  text,
+}) {
   const radius = (size - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
   const halfCircumference = circumference / 2;
 
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
       <Svg width={size} height={size}>
         <Circle
           stroke={color1}
@@ -37,27 +43,26 @@ export default function CollectionGraphicComponent({ size, strokeWidth, color1, 
           cy={size / 2}
           r={radius - strokeWidth / 2}
         />
-        <G x={size / 2} y={size / 2} textAnchor="middle" alignmentBaseline="central">
+        <G
+          x={size / 2}
+          y={size / 2}
+          textAnchor="middle"
+          alignmentBaseline="central"
+        >
           <Text
             fill="black"
             fontSize="18"
             fontWeight="bold"
-            textAnchor='start'
+            textAnchor="start"
             dy=".3em"
           >
             {text}
           </Text>
           <G x={-60} y={-10}>
-            <Path
-              d="M 0 -10 L -10 -20 L 10 -20 Z"
-              fill="black"
-            />
+            <Path d="M 0 -10 L -10 -20 L 10 -20 Z" fill="black" />
           </G>
           <G x={-20} y={-16}>
-            <Path
-              d="M 0 10 L -10 20 L 10 20 Z"
-              fill="black"
-            />
+            <Path d="M 0 10 L -10 20 L 10 20 Z" fill="black" />
           </G>
         </G>
       </Svg>
--- pages/Home/ProfilePageItem/CollectionsOfBrand.jsx
+++ pages/Home/ProfilePageItem/CollectionsOfBrand.jsx
@@ -1,15 +1,13 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import CollectionsItem from '../ProfilePages/profileComponents/CollectionsItem'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import CollectionsItem from "../ProfilePages/profileComponents/CollectionsItem";
 
-export default function CollectionsOfBrand({data}) {
+export default function CollectionsOfBrand({ data }) {
   return (
-   <ScrollView style={{paddingLeft:10,paddingRight:10}}>
-    {
-      data?.data?.collections.map((item,_i)=>(
-        <CollectionsItem item={item} showDots={true}/>
-      ))
-    }
-   </ScrollView>
-  )
+    <ScrollView style={{ paddingLeft: 10, paddingRight: 10 }}>
+      {data?.data?.collections?.map((item, _i) => (
+        <CollectionsItem item={item} showDots={true} />
+      ))}
+    </ScrollView>
+  );
 }
