fb49eb6d47e2de1e589b7213473ca89f8228cbb1
Kerem Bozmaz <kerembzmz44@gmail.com>
2024-11-20 12:28:25 +0300

    ana sayfa tasarımı düzenlendi

--- components/SliderItem.jsx
+++ components/SliderItem.jsx
@@ -120,8 +120,8 @@ export default function SliderItem({
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "transparent",
-    width: 55,
-    height: 55,
+    width: 60,
+    height: 60,
     flexWrap: "wrap",
     borderRadius: 50,
     margin: 6,
