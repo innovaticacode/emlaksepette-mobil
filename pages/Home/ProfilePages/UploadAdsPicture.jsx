7ed0d8fc204b53f647a44e463b39bb77f81d8d96
Kerem Bozmaz <kerembzmz44@gmail.com>
2024-11-21 16:56:06 +0300

    stiller düzeldi

--- pages/Home/ProfilePages/UploadAdsPicture.jsx
+++ pages/Home/ProfilePages/UploadAdsPicture.jsx
@@ -21,7 +21,11 @@ import Modal from "react-native-modal";
 import Icon3 from "react-native-vector-icons/MaterialIcons";
 import AwesomeAlert from "react-native-awesome-alerts";
 
-import { ALERT_TYPE, AlertNotificationRoot, Dialog } from "react-native-alert-notification";
+import {
+  ALERT_TYPE,
+  AlertNotificationRoot,
+  Dialog,
+} from "react-native-alert-notification";
 import AdsPictureList from "./AdsPictureList";
 export default function UploadAdsPicture() {
   const [choose, setchoose] = useState(false);
@@ -32,9 +36,7 @@ export default function UploadAdsPicture() {
     // Galeri iznini kontrol et ve iste
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (status !== "granted") {
-      Alert.alert(
-        "Galeriye erişim sağlamak için izin vermeniz gerekmektedir."
-      );
+      Alert.alert("Galeriye erişim sağlamak için izin vermeniz gerekmektedir.");
       return;
     }
 
@@ -74,10 +76,10 @@ export default function UploadAdsPicture() {
       setchoose(false);
     }
   };
-  const [loading, setloading] = useState(false)
+  const [loading, setloading] = useState(false);
 
   const getStoreBanner = () => {
-    setloading(true)
+    setloading(true);
     apiRequestGetWithBearer("institutional/store_banner")
       .then((res) => {
         setStoreBanners(res.data);
@@ -86,16 +88,16 @@ export default function UploadAdsPicture() {
         alert(err);
       })
       .finally(() => {
-        setloading(false)
+        setloading(false);
       });
   };
 
   useEffect(() => {
     getStoreBanner();
   }, []);
-  const [loadingUpdate, setloadingUpdate] = useState(false)
+  const [loadingUpdate, setloadingUpdate] = useState(false);
   const SendPictureToApı = () => {
-    setloadingUpdate(true)
+    setloadingUpdate(true);
     var dad = new FormData();
     dad.append("image", {
       uri:
@@ -104,245 +106,213 @@ export default function UploadAdsPicture() {
           : ımage?.uri.replace("file://", ""),
 
       type: ımage.mimeType,
-      name: ımage.fileName == null ? 'Image.jpeg' : ımage.fileName,
+      name: ımage.fileName == null ? "Image.jpeg" : ımage.fileName,
     });
     dad.append("order", StoreBanners.length + 1);
     apiRequestPostWithBearer("institutional/store_banner", dad)
-      .then((res) => {
-
-
-      })
+      .then((res) => {})
       .catch((err) => {
         alert(err);
-      }).finally(() => {
-        setloadingUpdate(false)
+      })
+      .finally(() => {
+        setloadingUpdate(false);
         setTimeout(() => {
           Dialog.show({
             type: ALERT_TYPE.SUCCESS,
             title: "Başarılı",
-            textBody: 'Mağaza Görseli Başarıyla Oluşturuldu',
+            textBody: "Mağaza Görseli Başarıyla Oluşturuldu",
             button: "Tamam",
 
             onHide: () => {
-              getStoreBanner()
-            }
+              getStoreBanner();
+            },
           });
-          setImage(null)
-
+          setImage(null);
         }, 500);
-      })
+      });
   };
-
 
   return (
     <AlertNotificationRoot>
-      {
-        loading ?
-          <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-            <ActivityIndicator color="#333" size={'large'} />
-          </View> :
-          <ScrollView
-            style={styles.container}
-            contentContainerStyle={styles.scrollContainer}
-          >
+      {loading ? (
+        <View
+          style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+        >
+          <ActivityIndicator color="#333" size={"large"} />
+        </View>
+      ) : (
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.scrollContainer}
+        >
+          {ımage && (
+            <View>
+              <Text style={styles.alertText}>
+                Seçili Görseli Silmek İçin Üzerine Tıklayın
+              </Text>
+            </View>
+          )}
 
-            {ımage && (
-              <View>
-                <Text
-                  style={{
-                    textAlign: "center",
-                    fontSize: 14,
-                    fontWeight: "600",
-                    color: "#333",
+          <View>
+            {StoreBanners.length <= 4 && (
+              <View style={{ gap: 10 }}>
+                <TouchableOpacity
+                  disabled={StoreBanners.length == 4}
+                  style={[
+                    styles.AddPicture,
+                    {
+                      borderColor: ımage ? "green" : "#EA2C2E",
+                      borderStyle: ımage ? "solid" : "dashed",
+                    },
+                  ]}
+                  onPress={() => {
+                    if (ımage) {
+                      setdeleteAlert(true);
+                    } else {
+                      setchoose(true);
+                    }
                   }}
                 >
-                  Seçili Görseli Silmek İçin Üzerine Tıklayın
-                </Text>
-              </View>
-            )}
-
-            <View>
-              {
-                StoreBanners.length <= 4 &&
-                <View style={{ gap: 10 }}>
-                  <TouchableOpacity
-
-                    style={[
-                      styles.AddPicture,
-                      {
-                        borderColor: ımage ? "green" : "#EA2C2E",
-                        borderStyle: ımage ? "solid" : "dashed",
-                      },
-                    ]}
-                    onPress={() => {
-                      if (ımage) {
-                        setdeleteAlert(true);
-                      } else {
-                        setchoose(true);
-                      }
-                    }}
-                  >
-                    {StoreBanners.length !== 4 ?
-
-                      ımage ? (
-                        <ImageBackground
-                          source={ımage}
-                          style={{ width: "100%", height: "100%" }}
-                          borderRadius={10}
-                        />
-                      ) : (
-                        <View style={styles.NonImageContent}>
-                          <View
-                            style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-                          >
-                            <Icon name="plus" color={"#EA2C2E"} size={20} />
-                            <Text
-                              style={{ fontSize: 14, color: "#EA2C2E", fontWeight: "600" }}
-                            >
-                              Reklam Görseli Ekle
-                            </Text>
-                          </View>
-                        </View>
-                      )
-                      : <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 ,paddingLeft:10,paddingRight:10,gap:10}}>
-                        <Text style={{ color: '#EA2B2E', fontWeight: '600', fontSize: 16 }}>Maximum Mağaza Görseli Sayısına Ulaştınız</Text>
-                        <Text style={{ color: '#EA2B2E', fontWeight: '600', fontSize: 13,textAlign:'center' }}>En fazla 4 adet banner ekleyebilirsiniz ekleyebilmek için görseli silnizi</Text>
-                      </View>
-                    }
-                  </TouchableOpacity>
-
-                  <View>
-                    <TouchableOpacity
-                      style={{ backgroundColor: "#EA2C2E", borderRadius: 8, padding: 8, opacity: StoreBanners.length == 4 || !ımage ? 0.5 : 1 }}
-                      onPress={SendPictureToApı}
-                      disabled={StoreBanners.length == 4 || !ımage ? true : false}
-                    >
-                      {
-                        loadingUpdate ?
-                          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                            <ActivityIndicator color="white" />
-                          </View>
-                          :
+                  {StoreBanners.length !== 4 ? (
+                    ımage ? (
+                      <ImageBackground
+                        source={ımage}
+                        style={{ width: "100%", height: "100%" }}
+                        borderRadius={10}
+                      />
+                    ) : (
+                      <View style={styles.NonImageContent}>
+                        <View
+                          style={styles.addPictureContainer}
+                        >
+                          <Icon name="plus" color={"#EA2C2E"} size={20} />
                           <Text
-                            style={{
-                              textAlign: "center",
-                              fontSize: 14,
-                              color: "white",
-                              fontWeight: "600",
-                            }}
+                            style={styles.adsPictureContainerText}
                           >
-                            Ekle
+                            Reklam Görseli Ekle
                           </Text>
-                      }
-
-                    </TouchableOpacity>
-                  </View>
-                </View>
-
-              }
-
-
-            </View>
-
-            {/* <View style={styles.alert}>
-                  <View style={styles.alertContent}>
-                    <View>
-                      <Icon name="questioncircleo" size={15} color={"#EA2C2E"} />
-                    </View>
-                    <View style={{ width: "100%" }}>
-                      <Text style={styles.alertText}>
-                        Bu alana eklenen görseller, her mağazanın kendi profilinde tanıtım
-                        ve reklam amaçlı slider'da gösterilecek. Mağazalarınızı doğru bir
-                        şekilde tanıtabilmek için bu adım çok önemlidir. Görsellerinizi
-                        yüklemek için aşağıdaki alana tıklayarak dosyalarınızı sürükleyip
-                        bırakabilir veya seçebilirsiniz. Görsellerinizi yükledikten sonra
-                        "Kaydet" butonuna basarak işlemi tamamlayabilirsiniz.
+                        </View>
+                      </View>
+                    )
+                  ) : (
+                    <View style={styles.textInUploadBtnContainer}>
+                      <Text style={[styles.textInUploadBtn, { fontSize: 16 }]}>
+                        Maximum Mağaza Görseli Sayısına Ulaştınız
+                      </Text>
+                      <Text style={[styles.textInUploadBtn,{ fontSize: 13, textAlign: "center" ,}]}>
+                        En fazla 4 adet banner ekleyebilirsiniz ekleyebilmek
+                        için görseli silnizi
                       </Text>
                     </View>
-                  </View>
-                </View> */}
+                  )}
+                </TouchableOpacity>
 
-            <View>
-              <AdsPictureList StoreBanners={StoreBanners} getStoreBanner={getStoreBanner} />
-            </View>
-
-            <AwesomeAlert
-              show={deleteAlert}
-              showProgress={false}
-              titleStyle={{
-                color: "#333",
-                fontSize: 13,
-                fontWeight: "700",
-                textAlign: "center",
-                margin: 5,
-              }}
-              title={"Seçilen Görseli Kaldır"}
-              messageStyle={{ textAlign: "center" }}
-              message={`Seçili Görseli Silmek İstediğinize Emin misiniz?`}
-              closeOnTouchOutside={true}
-              closeOnHardwareBackPress={false}
-              showCancelButton={true}
-              showConfirmButton={true}
-              cancelText="Vazgeç"
-              confirmText="Sil"
-              cancelButtonColor="#ce4d63"
-              confirmButtonColor="#1d8027"
-              onCancelPressed={() => {
-                setdeleteAlert(false);
-              }}
-              onConfirmPressed={() => {
-                setImage(null);
-                setdeleteAlert(false);
-              }}
-              confirmButtonTextStyle={{ marginLeft: 20, marginRight: 20 }}
-              cancelButtonTextStyle={{ marginLeft: 20, marginRight: 20 }}
-            />
-            <Modal
-              isVisible={choose}
-              style={styles.modal2}
-              animationIn={"slideInUp"}
-              animationOut={"slideOutDown"}
-              onBackdropPress={() => setchoose(false)}
-              swipeDirection={["down"]}
-              onSwipeComplete={() => setchoose(false)}
-            >
-              <View style={[styles.modalContent2, { paddingBottom: 10 }]}>
-                <View style={{ paddingTop: 10, alignItems: "center" }}>
+                <View>
                   <TouchableOpacity
                     style={{
-                      width: "15%",
-                      backgroundColor: "#c2c4c6",
-                      padding: 4,
-                      borderRadius: 50,
+                      backgroundColor: "#EA2C2E",
+                      borderRadius: 8,
+                      padding: 8,
+                      opacity: StoreBanners.length == 4 || !ımage ? 0.5 : 1,
                     }}
-                  ></TouchableOpacity>
-                </View>
-                <View style={{ padding: 20, gap: 35, marginBottom: 10 }}>
-                  <TouchableOpacity
-                    style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-                    onPress={pickImageFromGallery}
+                    onPress={SendPictureToApı}
+                    disabled={StoreBanners.length == 4 || !ımage ? true : false}
                   >
-                    <Icon3 name="photo" size={23} color={"#333"} />
-                    <Text style={{ fontSize: 14, color: "#333", fontWeight: "700" }}>
-                      Kütüphaneden Seç
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-                    onPress={takePhotoWithCamera}
-                  >
-                    <Icon3 name="add-a-photo" size={21} color={"#333"} />
-                    <Text style={{ fontSize: 14, color: "#333", fontWeight: "700" }}>
-                      Fotoğraf Çek
-                    </Text>
+                    {loadingUpdate ? (
+                      <View
+                        style={{
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <ActivityIndicator color="white" />
+                      </View>
+                    ) : (
+                      <Text
+                        style={styles.UploadBtnText}
+                      >
+                        Ekle
+                      </Text>
+                    )}
                   </TouchableOpacity>
                 </View>
               </View>
-            </Modal>
-          </ScrollView>
-      }
+            )}
+          </View>
 
+          <View>
+            <AdsPictureList
+              StoreBanners={StoreBanners}
+              getStoreBanner={getStoreBanner}
+            />
+          </View>
 
+          <AwesomeAlert
+            show={deleteAlert}
+            showProgress={false}
+            titleStyle={{
+              color: "#333",
+              fontSize: 13,
+              fontWeight: "700",
+              textAlign: "center",
+              margin: 5,
+            }}
+            title={"Seçilen Görseli Kaldır"}
+            messageStyle={{ textAlign: "center" }}
+            message={`Seçili Görseli Silmek İstediğinize Emin misiniz?`}
+            closeOnTouchOutside={true}
+            closeOnHardwareBackPress={false}
+            showCancelButton={true}
+            showConfirmButton={true}
+            cancelText="Vazgeç"
+            confirmText="Sil"
+            cancelButtonColor="#ce4d63"
+            confirmButtonColor="#1d8027"
+            onCancelPressed={() => {
+              setdeleteAlert(false);
+            }}
+            onConfirmPressed={() => {
+              setImage(null);
+              setdeleteAlert(false);
+            }}
+            confirmButtonTextStyle={{ marginLeft: 20, marginRight: 20 }}
+            cancelButtonTextStyle={{ marginLeft: 20, marginRight: 20 }}
+          />
+          <Modal
+            isVisible={choose}
+            style={styles.modal2}
+            animationIn={"slideInUp"}
+            animationOut={"slideOutDown"}
+            onBackdropPress={() => setchoose(false)}
+            swipeDirection={["down"]}
+            onSwipeComplete={() => setchoose(false)}
+          >
+            <View style={[styles.modalContent2, { paddingBottom: 10 }]}>
+              <View style={{ paddingTop: 10, alignItems: "center" }}>
+                <TouchableOpacity
+                  style={styles.modalTopBtn}
+                ></TouchableOpacity>
+              </View>
+              <View style={{ padding: 20, gap: 35, marginBottom: 10 }}>
+                <TouchableOpacity
+                  style={styles.chooseButtons}
+                  onPress={pickImageFromGallery}
+                >
+                  <Icon3 name="photo" size={23} color={"#333"} />
+                  <Text style={styles.chooseButtonsText}>Kütüphaneden Seç</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.chooseButtons}
+                  onPress={takePhotoWithCamera}
+                >
+                  <Icon3 name="add-a-photo" size={21} color={"#333"} />
+                  <Text style={styles.chooseButtonsText}>Fotoğraf Çek</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </ScrollView>
+      )}
     </AlertNotificationRoot>
   );
 }
@@ -409,4 +379,54 @@ const styles = StyleSheet.create({
       },
     }),
   },
+  chooseButtons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  chooseButtonsText: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "700",
+  },
+  alertText: {
+    textAlign: "center",
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#333",
+  },
+  textInUploadBtn: {
+    color: "#EA2B2E",
+    fontWeight: "600",
+  },
+  textInUploadBtnContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    gap: 10,
+  },
+  addPictureContainer:{
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  adsPictureContainerText:{
+    fontSize: 14,
+    color: "#EA2C2E",
+    fontWeight: "600",
+  },
+  UploadBtnText:{
+    textAlign: "center",
+    fontSize: 14,
+    color: "white",
+    fontWeight: "600",
+  },
+  modalTopBtn:{
+    width: "15%",
+    backgroundColor: "#c2c4c6",
+    padding: 4,
+    borderRadius: 50,
+  }
 });
