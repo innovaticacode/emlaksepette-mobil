2907f54c87b90ab42991794654f95b3942833024
Öztürk Şirin <ozturksirininfo@gmail.com>
2024-11-27 08:57:44 +0300

    update kerem

--- components/VerifyComponents/NextAndPrevButton.jsx
+++ components/VerifyComponents/NextAndPrevButton.jsx
@@ -1,61 +1,114 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const NextAndPrevButton = ({nextButtonPress,prevButtonPress,NextButtonDisabled,PrevButtonDisabled}) => {
-    console.log(NextButtonDisabled+ 'fdf')
+const NextAndPrevButton = ({
+  nextButtonPress,
+  prevButtonPress,
+  NextButtonDisabled,
+  PrevButtonDisabled,
+  step,
+  userType,
+}) => {
+  console.log(NextButtonDisabled + "fdf");
+  const [user, setuser] = useState({});
+  const SetStep = async () => {
+    try {
+      if (user?.access_token) {
+        // Gönderilecek JSON verisi
+        const payload = {
+          column_name: "banner_hex_code",
+          value: currentColor,
+        };
+
+        const response = await axios.post(
+          `https://private.emlaksepette.com/api/change-profile-value-by-column-name`,
+          payload, // JSON verisi doğrudan gönderiliyor
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              "Content-Type": "application/json", // Raw format için Content-Type
+            },
+          }
+        );
+
+        setTimeout(() => {
+          Dialog.show({
+            type: ALERT_TYPE.SUCCESS,
+            title: "Başarılı",
+            textBody: `Mağaza Renginiz Olşturuldu`,
+            button: "Tamam",
+          });
+        }, 500);
+      }
+    } catch (error) {
+      console.error("Post isteği başarısız", error);
+    }
+  };
   return (
     <View style={styles.containerButtons}>
-     <TouchableOpacity style={[styles.PrevButton]} onPress={()=>{
-        prevButtonPress()
-     }}>
-            <Text style={styles.PrevButtonText}>Önceki Adım</Text>
-     </TouchableOpacity>
+      {step != 1 && (
+        <TouchableOpacity
+          style={[styles.PrevButton]}
+          onPress={() => {
+            prevButtonPress();
+          }}
+        >
+          <Text style={styles.PrevButtonText}>Önceki Adım</Text>
+        </TouchableOpacity>
+      )}
 
-     <TouchableOpacity disabled={NextButtonDisabled}  style={[styles.NextButton]} onPress={()=>{
-        nextButtonPress()
-     }}>
-            <Text style={styles.NextButtonText}>Sonraki Adım</Text>
-     </TouchableOpacity>
+      <TouchableOpacity
+        disabled={NextButtonDisabled == false ? true : false}
+        style={[
+          styles.NextButton,
+          { opacity: NextButtonDisabled == false ? 0.5 : 1 },
+        ]}
+        onPress={() => {
+          nextButtonPress();
+        }}
+      >
+        <Text style={styles.NextButtonText}>Sonraki Adım</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default NextAndPrevButton
+export default NextAndPrevButton;
 
-const styles=StyleSheet.create({
-    NextButton:{
-        backgroundColor:'#EA2C2E',
-        padding:10,
-        width:'45%',
-        borderRadius:10
-    },
-    NextButtonText:{
-        color:'white',
-        fontSize:14,
-        fontWeight:'600',
-        textAlign:'center'
-    },
-    PrevButton:{
-        backgroundColor:'white',
-        padding:10,
-        width:'45%',
-        borderRadius:10,
-        borderWidth:1,
-        borderColor:'#EA2C2E'
-    },
-    PrevButtonText:{
-            color:'#EA2C2E',
-            fontSize:14,
-            fontWeight:'600',
-            textAlign:'center'
-    },
-    containerButtons:{
-        position:'absolute',
-        zIndex:1,
-        bottom:30,
-        flexDirection:'row',
-        width:'100%',
-        justifyContent:'space-around'
-    }
-
-})
+const styles = StyleSheet.create({
+  NextButton: {
+    backgroundColor: "#EA2C2E",
+    padding: 10,
+    width: "45%",
+    borderRadius: 10,
+  },
+  NextButtonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  PrevButton: {
+    backgroundColor: "white",
+    padding: 10,
+    width: "45%",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#EA2C2E",
+  },
+  PrevButtonText: {
+    color: "#EA2C2E",
+    fontSize: 14,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  containerButtons: {
+    position: "absolute",
+    zIndex: 1,
+    bottom: 30,
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+});
--- components/VerifyComponents/UpdateProfileImage.jsx
+++ components/VerifyComponents/UpdateProfileImage.jsx
@@ -1,17 +1,28 @@
-import { View, Text, StyleSheet, TouchableOpacity, Platform, ImageBackground } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import ColorPicker from 'react-native-wheel-color-picker'
-import Icon from 'react-native-vector-icons/AntDesign'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  ImageBackground,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import ColorPicker from "react-native-wheel-color-picker";
+import Icon from "react-native-vector-icons/AntDesign";
 import * as ImagePicker from "expo-image-picker";
 import Icon3 from "react-native-vector-icons/MaterialIcons";
-import  Modal from 'react-native-modal'
-import { getValueFor } from '../methods/user';
-import { apiUrl } from '../methods/apiRequest';
+import Modal from "react-native-modal";
+import { getValueFor } from "../methods/user";
+import { apiUrl, frontEndUriBase } from "../methods/apiRequest";
 
-import { ALERT_TYPE, AlertNotificationRoot,Dialog } from 'react-native-alert-notification';
-import axios from 'axios';
-import NextAndPrevButton from './NextAndPrevButton';
-const UpdateProfileImage = ({nextStep,prevStep}) => {
+import {
+  ALERT_TYPE,
+  AlertNotificationRoot,
+  Dialog,
+} from "react-native-alert-notification";
+import axios from "axios";
+import NextAndPrevButton from "./NextAndPrevButton";
+const UpdateProfileImage = ({ nextStep, prevStep }) => {
   const [currentColor, setCurrentColor] = useState(null);
   const [choose, setchoose] = useState(false);
   const [image, setImage] = useState(null);
@@ -36,7 +47,7 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
 
     if (!result.canceled) {
       setImage(result.assets[0]); // Seçilen fotoğrafı state'e kaydediyoruz
-      UploadProfile(result.assets[0])
+      UploadProfile(result.assets[0]);
       setchoose(false); // Modal'ı kapatıyoruz
     }
   };
@@ -50,7 +61,7 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
 
     if (!result.canceled) {
       setImage(result.assets[0]); // Çekilen fotoğrafı state'e kaydediyoruz
-      UploadProfile(result.assets[0])
+      UploadProfile(result.assets[0]);
       setchoose(false); // Modal'ı kapatıyoruz
     }
   };
@@ -58,11 +69,11 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
   useEffect(() => {
     getValueFor("user", setuser);
   }, []);
-  const UploadProfile= async (imageUpload) => {
+  const UploadProfile = async (imageUpload) => {
     const formData = new FormData();
     formData.append(
       `profile_image`,
-     imageUpload
+      imageUpload
         ? {
             uri:
               Platform.OS === "android"
@@ -72,7 +83,7 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
             name:
               imageUpload.name == null
                 ? "İmage.jpeg"
-                :imageUpload?.name?.slice(-3) == "pdf"
+                : imageUpload?.name?.slice(-3) == "pdf"
                 ? imageUpload?.name
                 : imageUpload?.fileName, // Sunucuya gönderilecek dosya adı
           }
@@ -82,14 +93,15 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
     try {
       if (user?.access_token) {
         const response = await axios.post(
-        
           `${apiUrl}upload-profile-image`,
-          formData, {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            "Content-Type": "multipart/form-data",
-          },
-        });
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
         setTimeout(() => {
           Dialog.show({
             type: ALERT_TYPE.SUCCESS,
@@ -108,10 +120,10 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
       if (user?.access_token) {
         // Gönderilecek JSON verisi
         const payload = {
-        column_name:'banner_hex_code',
-        value:currentColor
+          column_name: "banner_hex_code",
+          value: currentColor,
         };
-  
+
         const response = await axios.post(
           `https://private.emlaksepette.com/api/change-profile-value-by-column-name`,
           payload, // JSON verisi doğrudan gönderiliyor
@@ -122,7 +134,7 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
             },
           }
         );
-  
+
         setTimeout(() => {
           Dialog.show({
             type: ALERT_TYPE.SUCCESS,
@@ -136,153 +148,211 @@ const UpdateProfileImage = ({nextStep,prevStep}) => {
       console.error("Post isteği başarısız", error);
     }
   };
+  const [loading, setloading] = useState(false);
+  const [namFromGetUser, setnamFromGetUser] = useState([]);
+  const GetUserInfo = async () => {
+    setloading(true);
+    try {
+      if (user?.access_token && user) {
+        const userInfo = await axios.get(`${apiUrl}users/` + user?.id, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+          },
+        });
+        const userData = userInfo?.data?.user;
+        setnamFromGetUser(userData);
+        setCurrentColor(userData?.banner_hex_code);
+        setImage(userData?.profile_image);
+      }
+    } catch (error) {
+      console.error("Kullanıcı verileri güncellenirken hata oluştu:", error);
+    } finally {
+    }
+  };
+  useEffect(() => {
+    GetUserInfo();
+  }, [user]);
   return (
     <AlertNotificationRoot>
-     <View style={[styles.profileImageContainer,{backgroundColor:currentColor}]}>
-          <TouchableOpacity style={{width:80,height:80,backgroundColor:'#E4E4E8',borderRadius:50,alignItems:'center',justifyContent:'center'}}
-              onPress={()=>{
-                setchoose(true)
-              }}
-          >
-                {
-                  image ?
-                  <ImageBackground source={{uri:image?.uri}} style={{width:'100%',height:'100%'}} borderRadius={50}/>:
-                  <Icon name='adduser' size={30} color={'#737373'} />
-                }
-                  
-            
-          </TouchableOpacity>
-     </View>
-
-   
-
-
-     <View style={{paddingLeft:30,paddingRight:30,paddingTop:20,height:200}}>
-     <ColorPicker
-                color={currentColor}
-                swatchesOnly={false}
-                onColorChange={(color)=>setCurrentColor(color)}
-          
-                thumbSize={50}
-                sliderSize={20}
-                noSnap={true}
-                gapSize={0}
-                sliderHidden={true}
-                row={false}
-                swatchesLast={false}
-                swatches={true}
-                discrete={false}
-                useNativeDriver={true}
-                useNativeLayout={false}
+      <View
+        style={[
+          styles.profileImageContainer,
+          { backgroundColor: currentColor },
+        ]}
+      >
+        <TouchableOpacity
+          style={{
+            width: 80,
+            height: 80,
+            backgroundColor: "#E4E4E8",
+            borderRadius: 50,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => {
+            setchoose(true);
+          }}
+        >
+          {image || image?.uri ? (
+            image?.uri ? (
+              <ImageBackground
+                source={{
+                  uri: image.uri,
+                }}
+                style={{ width: "100%", height: "100%" }}
+                borderRadius={50}
               />
-            
-     </View>
-                <View style={{alignItems:'center',justifyContent:'center',padding:15,paddingTop:40}}>
-                <TouchableOpacity style={styles.butonChoose} onPress={()=>{
-                  UploadBannerHexCode()
-                }}>
-                <Text style={styles.butonChooseText}>Seç</Text>
-              </TouchableOpacity>   
-              </View>  
-  
-                  
-     <Modal
-              isVisible={choose}
-              style={styles.modal2}
-              animationIn={"fadeInDown"}
-              animationOut={"fadeOutDown"}
-              onBackdropPress={() => setchoose(false)}
-              swipeDirection={["down"]}
-              onSwipeComplete={() => setchoose(false)}
+            ) : (
+              <ImageBackground
+                source={{
+                  uri: `${frontEndUriBase}storage/profile_images/${image}`,
+                }}
+                style={{ width: "100%", height: "100%" }}
+                borderRadius={50}
+              />
+            )
+          ) : (
+            <Icon name="adduser" size={30} color={"#737373"} />
+          )}
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          paddingLeft: 30,
+          paddingRight: 30,
+          paddingTop: 20,
+          height: 200,
+        }}
+      >
+        <ColorPicker
+          color={currentColor}
+          swatchesOnly={false}
+          onColorChange={(color) => setCurrentColor(color)}
+          thumbSize={50}
+          sliderSize={20}
+          noSnap={true}
+          gapSize={0}
+          sliderHidden={true}
+          row={false}
+          swatchesLast={false}
+          swatches={true}
+          discrete={false}
+          useNativeDriver={true}
+          useNativeLayout={false}
+        />
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 15,
+          paddingTop: 40,
+        }}
+      >
+        <TouchableOpacity
+          style={styles.butonChoose}
+          onPress={() => {
+            UploadBannerHexCode();
+          }}
+        >
+          <Text style={styles.butonChooseText}>Seç</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Modal
+        isVisible={choose}
+        style={styles.modal2}
+        animationIn={"fadeInDown"}
+        animationOut={"fadeOutDown"}
+        onBackdropPress={() => setchoose(false)}
+        swipeDirection={["down"]}
+        onSwipeComplete={() => setchoose(false)}
+      >
+        <View style={[styles.modalContent2, { paddingBottom: 10 }]}>
+          <View style={{ paddingTop: 10, alignItems: "center" }}>
+            <TouchableOpacity
+              style={{
+                width: "15%",
+                backgroundColor: "#c2c4c6",
+                padding: 4,
+                borderRadius: 50,
+              }}
+            ></TouchableOpacity>
+          </View>
+          <View style={{ padding: 20, gap: 35, marginBottom: 10 }}>
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+              onPress={pickImage}
             >
-              <View style={[styles.modalContent2, { paddingBottom: 10 }]}>
-                <View style={{ paddingTop: 10, alignItems: "center" }}>
-                  <TouchableOpacity
-                    style={{
-                      width: "15%",
-                      backgroundColor: "#c2c4c6",
-                      padding: 4,
-                      borderRadius: 50,
-                    }}
-                  ></TouchableOpacity>
-                </View>
-                <View style={{ padding: 20, gap: 35, marginBottom: 10 }}>
-                  <TouchableOpacity
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 10,
-                    }}
-                    onPress={pickImage}
-                  >
-                    <Icon3 name="photo" size={23} color={"#333"} />
-                    <Text
-                      style={{ fontSize: 14, color: "#333", fontWeight: "700" }}
-                    >
-                      Kütüphaneden Seç
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 10,
-                    }}
-                    onPress={takePhoto}
-                  >
-                    <Icon3 name="add-a-photo" size={21} color={"#333"} />
-                    <Text
-                      style={{ fontSize: 14, color: "#333", fontWeight: "700" }}
-                    >
-                      Fotoğraf Çek
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      gap: 10,
-                    }}
-                    onPress={()=>{
-                      setchoose(false)
-                      setImage(null)
-                    }} // Yalnızca yerelde kaldırmak isterseniz bu işlevi kullanın
-                    // onPress={removeProfileImageFromServer} // Sunucudan da kaldırmak isterseniz bu işlevi kullanın
-                  >
-                    <Icon3
-                      name="restore-from-trash"
-                      size={22}
-                      color={"#d83131"}
-                    />
-                    <Text
-                      style={{
-                        fontSize: 14,
-                        color: "#d83131",
-                        fontWeight: "700",
-                      }}
-                    >
-                      Fotoğrafı Kaldır
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </Modal>
-         
-            <NextAndPrevButton nextButtonPress={nextStep} prevButtonPress={prevStep} PrevButtonDisabled={true} NextButtonDisabled={(image && currentColor) ? true : false }/>
-           
-           
+              <Icon3 name="photo" size={23} color={"#333"} />
+              <Text style={{ fontSize: 14, color: "#333", fontWeight: "700" }}>
+                Kütüphaneden Seç
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+              onPress={takePhoto}
+            >
+              <Icon3 name="add-a-photo" size={21} color={"#333"} />
+              <Text style={{ fontSize: 14, color: "#333", fontWeight: "700" }}>
+                Fotoğraf Çek
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+              onPress={() => {
+                setchoose(false);
+                setImage(null);
+              }} // Yalnızca yerelde kaldırmak isterseniz bu işlevi kullanın
+              // onPress={removeProfileImageFromServer} // Sunucudan da kaldırmak isterseniz bu işlevi kullanın
+            >
+              <Icon3 name="restore-from-trash" size={22} color={"#d83131"} />
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: "#d83131",
+                  fontWeight: "700",
+                }}
+              >
+                Fotoğrafı Kaldır
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <NextAndPrevButton
+        nextButtonPress={nextStep}
+        prevButtonPress={prevStep}
+        PrevButtonDisabled={true}
+        NextButtonDisabled={image && currentColor ? true : false}
+        step={1}
+        userType={user?.type}
+      />
     </AlertNotificationRoot>
-  )
-}
+  );
+};
 
-export default UpdateProfileImage
-const styles=StyleSheet.create({
-  profileImageContainer:{
-    width:'100%',
-    height:200,
-    alignItems:'center',
-    justifyContent:'center'
-
+export default UpdateProfileImage;
+const styles = StyleSheet.create({
+  profileImageContainer: {
+    width: "100%",
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
   },
   modal2: {
     justifyContent: "flex-end",
@@ -308,16 +378,16 @@ const styles=StyleSheet.create({
       },
     }),
   },
-  butonChoose:{
-    backgroundColor:'#EA2C2E',
-    padding:10,
-    width:'70%',
-    borderRadius:10
+  butonChoose: {
+    backgroundColor: "#EA2C2E",
+    padding: 10,
+    width: "70%",
+    borderRadius: 10,
   },
-  butonChooseText:{
-    color:'white',
-    fontSize:13,
-    fontWeight:'600',
-    textAlign:'center'
-  }
-})
+  butonChooseText: {
+    color: "white",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+});
