6651851b546909ee477e43ce50c48e450c4a66e8
Aykut <aykutaltnsk@gmail.com>
2024-12-07 10:25:24 +0300

    Üye ol ( bireysel, kurumsak ) giriş yap e-posta düzeltildi - Utils index'in içinden çagırılıyor

--- pages/Home/Login&Register/Company.jsx
+++ pages/Home/Login&Register/Company.jsx
@@ -22,8 +22,7 @@ import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 import { ActivityIndicator } from "react-native-paper";
 import { apiUrl } from "../../../components/methods/apiRequest";
 import { Dialog } from "react-native-alert-notification";
-import { sanitizeEmail } from "../../../utils/formatEmail";
-
+import { sanitizeEmail } from "../../../utils";
 export default function Company() {
   const Navigation = useNavigation();
   const [selectedIndexRadio, setIndexRadio] = useState(0);
